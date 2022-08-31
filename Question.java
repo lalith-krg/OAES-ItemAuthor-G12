782c7822fe40ede8e713a292164fa1df562c7f58
Tarun <82649090+Octopops2442@users.noreply.github.com>
2022-08-31 20:20:23 +0530

    Delete Question.java

--- Question.java
+++
@@ -1,8 +0,0 @@
-abstract class Question{
-    public abstract int getId();
-    public abstract String getQues();
-    public abstract void initialize(int ID, String subject);
-    public abstract void addQ(String q);
-    public abstract void storeFile(String path);
-    public abstract void setVersion(int ver);
-}
