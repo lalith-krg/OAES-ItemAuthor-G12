0710ee9b705a143d5db83eab89c2bd91318fad73
Tarun <82649090+Octopops2442@users.noreply.github.com>
2022-08-31 20:28:28 +0530

    Update connect.java

--- connect.java
+++ connect.java
@@ -5,7 +5,7 @@ public class connect {
     //I kept the database name as OAES so it is used in the url below. Change it to your own database name.
 
     private final static String url = "jdbc:mysql://localhost:3306/OAES"; // Dont change this, This is generally default.
-    private final static String username = ""; // Change the password to your respective database
+    private final static String username = ""; // Change the username to your respective database
     private final static String password = ""; // Change the password to your respective database
 
     public static Connection getConnection() {
